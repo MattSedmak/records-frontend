c06d5a4721b6b54e43b1ab16b92f6d58d6819627
MattSedmak <sedmakmatt@gmail.com>
2021-12-21 20:44:33 +0100

    Fixed nav toggle hover

--- components/header/NavToggle.tsx
+++ components/header/NavToggle.tsx
@@ -13,8 +13,8 @@ export const NavToggle = ({ open, onClick }: NavtoggleProps) => {
       className={classNames(styles.navToggle, { [styles.open]: open })}
       onClick={onClick}
     >
-      <div className={styles.icon}></div>
       <span>{open ? 'close' : 'menu'}</span>
+      <div className={styles.icon}></div>
     </div>
   );
 };
