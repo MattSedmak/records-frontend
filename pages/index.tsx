37c380b27d7295e1c567f412d2dd53ee45146e83
MattSedmak <sedmakmatt@gmail.com>
2021-12-21 22:43:13 +0100

    Added logo to start, fixed mobile to header & footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,21 @@ import { Layout } from '@components/layout';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import styles from './index.module.scss';
+
+import squareLogo from '@/public/images/Square-logo-dark.svg';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
-  return <Layout>{/* <div>Hello World</div> */}</Layout>;
+  return (
+    <Layout>
+      <Link href='/'>
+        <a className={styles.logoWrapper}>
+          <Image src={squareLogo} width={600} height={600} />
+        </a>
+      </Link>
+    </Layout>
+  );
 };
 
 export default Home;
