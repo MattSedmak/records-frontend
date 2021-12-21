89d94a4b03a1879737b5f45f49ca559521917a65
MattSedmak <sedmakmatt@gmail.com>
2021-12-21 21:34:57 +0100

    Added footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link';
+import styles from './Footer.module.scss';
+
 export const Footer = () => {
-  return <div>Footer works</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <span>&copy; 2021</span>
+      </div>
+
+      <Link href='/'>
+        <a className={styles.footerLink}>
+          <span>About</span>
+        </a>
+      </Link>
+    </div>
+  );
 };
