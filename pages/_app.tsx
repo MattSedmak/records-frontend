b09d4b573f18cfbb76da0efdf34ad1e53f3d03ab
MattSedmak <sedmakmatt@gmail.com>
2022-01-17 15:59:41 +0100

    added page loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,25 +12,25 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [isAnimating, setIsAnimating] = useState(false);
   const router = useRouter();
 
-  // add custom route change animation
-  // useEffect(() => {
-  //   const handleStart = () => setIsAnimating(true);
-  //   const handleStop = () => setIsAnimating(false);
+  //add custom route change animation
+  useEffect(() => {
+    const handleStart = () => setIsAnimating(true);
+    const handleStop = () => setIsAnimating(false);
 
-  //   router.events.on('routeChangeStart', handleStart);
-  //   router.events.on('routeChangeComplete', handleStop);
-  //   router.events.on('routeChangeError', handleStop);
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
 
-  //   return () => {
-  //     router.events.off('routeChangeStart', handleStart);
-  //     router.events.off('routeChangeComplete', handleStop);
-  //     router.events.off('routeChangeError', handleStop);
-  //   };
-  // }, [router]);
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ProgressBar isAnimating={isAnimating} /> */}
+      <ProgressBar isAnimating={isAnimating} />
       <AnimatePresence exitBeforeEnter onExitComplete={() => window.scrollTo(0, 0)}>
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
